85176982010ef68f03a0390d197e87344572b088
Omar <omar.lopesino@metadrop.net>
2020-03-09 17:58:16 +0100

    SETTINGS - Only setup database when environment variables exists II.

--- web/sites/default/ci.settings.local.php
+++ web/sites/default/ci.settings.local.php
@@ -1,15 +1,18 @@
 <?php
 
-$databases['default']['default'] = [
-  'database' => getenv('DB_NAME'),
-  'username' => getenv('DB_USER'),
-  'password' => getenv('DB_PASSWORD'),
-  'prefix' => '',
-  'host' => getenv('DB_HOST'),
-  'port' => getenv('DB_PORT'),
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\' . getenv('DB_DRIVER'),
-  'driver' => getenv('DB_DRIVER'),
-];
+// Only setup database when env vars are available.
+if (getenv('DB_NAME') != NULL) {
+  $databases['default']['default'] = [
+    'database' => getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'prefix' => '',
+    'host' => getenv('DB_HOST'),
+    'port' => getenv('DB_PORT'),
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\' . getenv('DB_DRIVER'),
+    'driver' => getenv('DB_DRIVER'),
+  ];
+}
 
 $config['config_split.config_split.ci']['status'] = TRUE;
-$config['stage_file_proxy.settings']['origin'] = 'https://example.net';
+$config['stage_file_proxy.settings']['origin'] = 'https://metadrop.net';
--- web/sites/default/example.settings.local.php
+++ web/sites/default/example.settings.local.php
@@ -5,16 +5,19 @@
  * Default configuration for local environments.
  */
 
-$databases['default']['default'] = [
-  'database' => getenv('DB_NAME'),
-  'username' => getenv('DB_USER'),
-  'password' => getenv('DB_PASSWORD'),
-  'prefix' => '',
-  'host' => getenv('DB_HOST'),
-  'port' => getenv('DB_PORT'),
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\' . getenv('DB_DRIVER'),
-  'driver' => getenv('DB_DRIVER'),
-];
+// Only setup database when env vars are available.
+if (getenv('DB_NAME') != NULL) {
+  $databases['default']['default'] = [
+    'database' => getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'prefix' => '',
+    'host' => getenv('DB_HOST'),
+    'port' => getenv('DB_PORT'),
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\' . getenv('DB_DRIVER'),
+    'driver' => getenv('DB_DRIVER'),
+  ];
+}
 
 $config['config_split.config_split.local']['status'] = TRUE;
-$config['stage_file_proxy.settings']['origin'] = 'https://example.net';
+$config['stage_file_proxy.settings']['origin'] = 'https://metadrop.net';
