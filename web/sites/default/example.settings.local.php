92b119ef0ca883346f1cfafe1dae0a50838c3877
Omar <omar.lopesino@metadrop.net>
2020-03-09 12:00:42 +0100

    SETTINGS LOCAL - Fix database configuration.

--- web/sites/default/example.settings.local.php
+++ web/sites/default/example.settings.local.php
@@ -1,7 +1,12 @@
 <?php
 
+/**
+ * @file
+ * Default configuration for local environments.
+ */
+
 $databases['default']['default'] = [
-  'database' => getenv('DB_HOST'),
+  'database' => getenv('DB_NAME'),
   'username' => getenv('DB_USER'),
   'password' => getenv('DB_PASSWORD'),
   'prefix' => '',
