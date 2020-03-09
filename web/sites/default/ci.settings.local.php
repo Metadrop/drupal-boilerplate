49753fedb6b0c3afbc4525b9de0e0fc311cbf3e9
Omar <omar.lopesino@metadrop.net>
2020-03-09 17:42:00 +0100

    BUGFIX: DB_HOST -> DB_NAME

--- web/sites/default/ci.settings.local.php
+++ web/sites/default/ci.settings.local.php
@@ -1,7 +1,7 @@
 <?php
 
 $databases['default']['default'] = [
-  'database' => getenv('DB_HOST'),
+  'database' => getenv('DB_NAME'),
   'username' => getenv('DB_USER'),
   'password' => getenv('DB_PASSWORD'),
   'prefix' => '',
