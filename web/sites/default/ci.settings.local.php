2c5c02bcbf911ba18ad76b086ff853fc678edeb6
Omar <omar.lopesino@metadrop.net>
2020-03-09 11:45:14 +0100

    CI - Add settings.local for ci environments.

---
+++ web/sites/default/ci.settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+$databases['default']['default'] = [
+  'database' => getenv('DB_HOST'),
+  'username' => getenv('DB_USER'),
+  'password' => getenv('DB_PASSWORD'),
+  'prefix' => '',
+  'host' => getenv('DB_HOST'),
+  'port' => getenv('DB_PORT'),
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\' . getenv('DB_DRIVER'),
+  'driver' => getenv('DB_DRIVER'),
+];
+
+$config['config_split.config_split.ci']['status'] = TRUE;
+$config['stage_file_proxy.settings']['origin'] = 'https://example.net';
