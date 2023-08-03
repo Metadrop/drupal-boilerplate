778f92e76d48b5f53f4d2f8e5141d0200e6d3d32
Omar <omar.lopesino@metadrop.net>
2023-08-03 14:06:59 +0200

    Base production tests

---
+++ tests/environment/pro/behat/bootstrap/FeatureContext.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Behat Feature Context file.
+ */
+
+use Behat\Mink\Driver\Selenium2Driver;
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Hook\Scope\BeforeStepScope;
+
+/**
+ * Main context for deploy tests.
+ */
+class FeatureContext extends MinkContext {
+
+}
