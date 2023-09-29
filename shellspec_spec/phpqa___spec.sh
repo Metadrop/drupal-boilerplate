31de2b6629414a53974c98212b3a2bc4c27b8ae9
Ricardo Sanz <ricardo@metadrop.net>
2023-09-29 15:03:10 +0200

    TESTS - Discard phpqa error output
    phpqa outputs info in the stderr but it is not relevant

--- shellspec_spec/phpqa___spec.sh
+++ shellspec_spec/phpqa___spec.sh
@@ -8,6 +8,6 @@ Describe 'Phpqa' phpqa static-code-analisys
     When run command docker-compose exec php phpqa
     The status should be success
     The output should include 'No failed tools'
+    The error should not include 'This is a dummy text to discard standard error output'
   End
 End
-
