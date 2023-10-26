ba5685f589e5a9e8d1bd78354a60e06501c7b5ab
Omar <omar.lopesino@metadrop.net>
2023-10-26 14:00:51 +0200

    SEt file_private path

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -514,7 +514,7 @@ $settings['update_free_access'] = FALSE;
  * See https://www.drupal.org/documentation/modules/file for more information
  * about securing private files.
  */
-# $settings['file_private_path'] = '';
+$settings['file_private_path'] = '../private-files';
 
 /**
  * Temporary file path:
