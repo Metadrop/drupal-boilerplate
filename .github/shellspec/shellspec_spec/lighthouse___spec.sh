ae74d11a5b1b9b78ef681021a7009c1e32d87f3b
Ricardo Sanz <ricardo@metadrop.net>
2024-01-25 23:31:44 +0100

    Lightouse outputs part of the test results in the error descriptor even when no errors

--- .github/shellspec/shellspec_spec/lighthouse___spec.sh
+++ .github/shellspec/shellspec_spec/lighthouse___spec.sh
@@ -5,6 +5,7 @@ Describe 'Lighthouse' lighthouse
     The status should be success
     The output should include "Healthcheck passed!"
     The output should include "Done running Lighthouse!"
+    The error should include "All results processed!"
     The path 'reports/lighthouse/_.report.html' should be file
     The path 'reports/lighthouse/_.report.json' should be file
     The path 'reports/lighthouse/manifest.json' should be file
