bff202f186e49021e7fdd9fab3cf79dcf04c3135
Omar <omar.lopesino@metadrop.net>
2024-03-19 12:42:10 +0100

    Add -T parameters to make create-database works on CD/CI

--- scripts/create-database.sh
+++ scripts/create-database.sh
@@ -8,4 +8,4 @@ PROJECT_ROOT="./"
 MYSQL_ROOT_PASS=`egrep DB_ROOT_PASSWORD ${PROJECT_ROOT}/.env | sed s/DB_ROOT_PASSWORD=//`
 MYSQL_HOST=`egrep DB_HOST ${PROJECT_ROOT}/.env | sed s/DB_HOST=//`
 
-docker compose exec mariadb mysql -u root -p${MYSQL_ROOT_PASS} -h ${MYSQL_HOST} -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO  'drupal'@'%';"
+docker compose exec -T mariadb mysql -u root -p${MYSQL_ROOT_PASS} -h ${MYSQL_HOST} -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO  'drupal'@'%';"
